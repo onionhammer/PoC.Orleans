44984f4e448e7ac638a5552783b8205d43f3667e
Erik O'Leary <erik.m.oleary@gmail.com>
2018-09-17 15:49:31 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoC.Orleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
