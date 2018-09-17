7ba8c0a12614efccd2024049cad163117083a331
Erik O'Leary <erik.m.oleary@gmail.com>
2018-09-17 17:04:01 -0500

    Run silo as a HostedServices

--- PoC.Silo/Program.cs
+++ PoC.Silo/Program.cs
@@ -1,29 +1,54 @@
 ﻿using System;
+using System.Runtime.Loader;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Orleans;
 using Orleans.Hosting;
 using PoC.Grains.Implementation;
 
 namespace PoC.Silo
 {
-    class Program
+    class Program : IHostedService
     {
+        readonly ISiloHost silo;
+
         static async Task Main(string[] args)
         {
-            // Start silo
-            var silo = new SiloHostBuilder()
+            var hostBuilder = new HostBuilder()
+                .ConfigureServices((HostBuilderContext, services) =>
+                {
+                    services.AddScoped<IHostedService, Program>();
+                });
+
+            await hostBuilder.RunConsoleAsync();
+        }
+
+        public Program()
+        {
+            silo = new SiloHostBuilder()
                 .UseLocalhostClustering()
-                .ConfigureApplicationParts(parts => 
+                .ConfigureApplicationParts(parts =>
                     parts.AddApplicationPart(typeof(DeviceGrain).Assembly).WithReferences()
                 )
                 .Build();
+        }
 
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
             Console.WriteLine("Starting silo...");
+            await silo.StartAsync(cancellationToken);
+        }
 
-            using (silo) 
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (silo != null)
             {
-                await silo.StartAsync();
+                Console.WriteLine("Stopping silo...");
+                await silo.StopAsync(cancellationToken);
                 await silo.Stopped;
+                Console.WriteLine("Stopped gracefully");
             }
         }
     }
